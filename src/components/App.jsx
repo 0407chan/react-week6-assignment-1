a4db233a6f38f754306025dfaca7aa35155fe6b5
이찬호 <0407chan@naver.com>
2022-04-25 23:39:40 +0900

    App에 라우트 반영

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,21 @@
-import RestaurantPage from '../Pages/RestaurantsPage';
+import {
+  BrowserRouter, Routes, Route,
+} from 'react-router-dom';
+
+import RestaurantsPage from '../Pages/RestaurantsPage';
+import HomePage from '../Pages/HomePage';
+import AboutPage from '../Pages/AboutPage';
+import NotFoundPage from '../Pages/NotFoundPage';
 
 export default function App() {
   return (
-    <RestaurantPage />
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/restaurants" element={<RestaurantsPage />} />
+        <Route path="/*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
