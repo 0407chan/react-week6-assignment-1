bd3dd8ad1ce8cb5c5dc2692dc11a7aff60f8efa2
이찬호 <0407chan@naver.com>
2022-04-25 23:43:14 +0900

    리팩토링. HomePage에 MemoryRouter적용

--- src/Pages/HomePage.test.jsx
+++ src/Pages/HomePage.test.jsx
@@ -1,9 +1,14 @@
 import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 
 import HomePage from './HomePage';
 
 describe('HomePage', () => {
   it('HomePage를 출력한다.', () => {
-    render(<HomePage />);
+    render(
+      <MemoryRouter>
+        <HomePage />
+      </MemoryRouter>,
+    );
   });
 });
